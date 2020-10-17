c3d6381d6dbd5fe6e4e1684532596b1b9266e180
A_ER_Y <erayozcan57@gmail.com>
2020-10-17 15:22:08 +0200

    made exercise 5.1

--- src/be/pxl/ja/oefening1/TestWorkingPlace.java
+++ src/be/pxl/ja/oefening1/TestWorkingPlace.java
@@ -6,5 +6,12 @@ public class TestWorkingPlace {
 		Car car = new Car();
 		CircularSaw circularSaw = new CircularSaw();
 		Bike bike = new Bike();
+
+        WorkingPlace<Car> workingPlace = new WorkingPlace<>();
+
+        workingPlace.startFixing(car);
+        workingPlace.finished();
+        System.out.println(workingPlace.getNumberOfThingsFixed());
+        System.out.println(WorkingPlaceUtility.getScore(workingPlace));
 	}
 }
